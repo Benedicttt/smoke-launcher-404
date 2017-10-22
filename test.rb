e884cd0a1b370d6ae4dec5d745108b4bf69a65be
Sergey <s.borisov@binpartner.com>
2017-10-22 22:53:52 +0300

    fix

--- test.rb
+++ test.rb
@@ -1,22 +1,24 @@
-require 'rubygems'
-require 'headless'
-require 'selenium-webdriver'
 
-Headless.ly do
-  driver = Selenium::WebDriver.for :chrome
-  driver.navigate.to 'http://google.com'
-  puts driver.title
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+  @driver = Selenium::WebDriver.for :firefox
 end
 
-require 'rubygems'
-require 'headless'
-require 'selenium-webdriver'
+def teardown
+  @driver.quit
+end
 
-headless = Headless.new
-headless.start
+def run
+  setup
+  yield
+  teardown
+end
 
-driver = Selenium::WebDriver.for :firefox
-driver.navigate.to 'http://google.com'
-puts driver.title
-
-headless.destroy
+run do
+  @driver.get 'http://the-internet.herokuapp.com'
+  expect(@driver.title).to eql 'The Internet'
+  @driver.save_screenshot 'headless.png'
+end
