b326e55d1c3b518ce6575a0dcfd2a54be63dbb33
Sergey <s.borisov@binpartner.com>
2017-11-17 14:33:31 +0300

    fix page display copntents and create generate features file for cucumber

--- app/controllers/binomo/content_page_controller.rb
+++ app/controllers/binomo/content_page_controller.rb
@@ -6,12 +6,34 @@ class Binomo::ContentPageController < ApplicationController
 
   def create
     scenarios = params["generator"]["scenario"]&.map {|f| @config[f]}.reject{ |x| x.nil?}
-    File.open("./features/content-page.feature", 'w+'){|f| f.puts("Feature: SMoke test generate #{Time.now}\n Scenario: Run step\n Given Pid process\n  ")}
-    File.open("./features/content-page.feature", 'a'){|f| f.puts(scenarios.join("\n"))}
+    settings = params["generator"]["scenario"]&.map {|f| @settings[f]}.reject{ |x| x.nil?}
+    content = params["generator"]["scenario"]&.map {|f| @content[f]}.reject{ |x| x.nil?}
+    faq = params["generator"]["scenario"]&.map {|f| @faq[f]}.reject{ |x| x.nil?}
+    sandbox = params["generator"]["scenario"]&.map {|f| @sandbox[f]}.reject{ |x| x.nil?}
+    offers = params["generator"]["scenario"]&.map {|f| @offers[f]}.reject{ |x| x.nil?}
+
+    File.open("./features/content-page.feature", 'w+') do |f|
+      f.puts("Feature: SMoke test generate #{Time.now}\n Scenario: Run step\n Given Pid process\n  ")
+    end
+
+    File.open("./features/content-page.feature", 'a') do |f|
+      f.puts(scenarios.join("\n"))
+      f.puts(settings.join("\n"))
+      f.puts(content.join("\n"))
+      f.puts(faq.join("\n"))
+      f.puts(sandbox.join("\n"))
+      f.puts(offers.join("\n"))
+    end
     p params["generator"]
+
   end
 
   def set_params
-    @config = YAML.load_file(Rails.root.join('./config/scenarios/contents.yml'))
+    @config   = YAML.load_file(Rails.root.join('./config/scenarios/contents.yml'))
+    @settings = YAML.load_file(Rails.root.join('./config/scenarios/contents.yml'))['settings']
+    @content  = YAML.load_file(Rails.root.join('./config/scenarios/contents.yml'))['content']
+    @faq      = YAML.load_file(Rails.root.join('./config/scenarios/contents.yml'))['content']['faq']
+    @sandbox   = YAML.load_file(Rails.root.join('./config/scenarios/contents.yml'))['sandbox']
+    @offers   = YAML.load_file(Rails.root.join('./config/scenarios/contents.yml'))['offers']
   end
 end
---
+++ features/content-page.feature
@@ -0,0 +1,10 @@
+Feature: SMoke test generate 2017-11-17 14:00:39 +0300
+ Scenario: Run step
+ Given Pid process
+  
+	Given Activate all templates
+	Given Include provider Eccompay
+
+
+
+
