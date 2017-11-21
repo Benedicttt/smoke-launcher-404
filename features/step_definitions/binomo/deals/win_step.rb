6507bae037c3af76d0c6caad65d994038631260c
Sergey <s.borisov@binpartner.com>
2017-11-21 18:50:42 +0300

    fix

--- features/step_definitions/binomo/deals/win_step.rb
+++ features/step_definitions/binomo/deals/win_step.rb
@@ -1,21 +1,13 @@
 When /^Make a deals asset GOL\/OTC and win bet$/ do
     sleep 3
-    $deals.find_elements(:css, ".dropdown-caret")[2].click
+    # $deals.find_elements(:css, ".dropdown-caret")[2].click
+    $deals.find_element(:css, "div.b-unit-dropdown-balance").click
     sleep 1
     find_and_click_selectors('span', 'FOR/HEIGHT', $deals)
     sleep 5
 
     ENV['count_deals_sum'].to_i.times { sleep 0.1; $deals.find_elements(:css, ".btn-control-sm")[0].click}
     $deals.find_elements(:css, ".btn-control-sm")[1].click
-
-    # if "#{ENV['turbo']}" == "true"
-    #   $deals.find_element(:xpath, "//*[@id=\"eur-graphs\"]/div/div/aside[2]/div/form/div[2]/div/div/ul/li[1]/a")
-    # end
-    #
-    # if "#{ENV['binary']}" == "true"
-    #   $deals.find_element(:xpath, "//*[@id=\"eur-graphs\"]/div/div/aside[2]/div/form/div[2]/div/div/ul/li[7]/a")
-    # end
-
 
     ENV['count_deals'].to_i.times  do |i|
       sleep ENV['between'].to_i
