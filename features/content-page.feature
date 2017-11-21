963fbd7491563dfbab9ebe38ec88cf31d6ed08e0
Sergey <s.borisov@binpartner.com>
2017-11-21 14:15:49 +0300

    fix faq

--- features/content-page.feature
+++ features/content-page.feature
@@ -1,12 +1,12 @@
-Feature: SMoke test generate 2017-11-21 12:55:22 +0300
+Feature: SMoke test generate 2017-11-21 14:15:30 +0300
  Scenario: Run step
  Given Pid process
   
 
 
-	Given Add new press articles
 
-	Given Add ticket category
+	Given Update first group
+	Given Update and set first group by question
 
 
 
--- features/step_definitions/crm/contents/old.rb
+++ features/step_definitions/crm/contents/old.rb
@@ -1,7 +1,3 @@
-
-
-
-
 Given /^Update all advises$/ do
   authorization_by_crm(DRIVER)
   DRIVER.get CommonSetting[:url_page_crm] + ""
@@ -10,21 +6,6 @@ Given /^Update all advises$/ do
 
 end
 
-Given /^Update first group$/ do
-  authorization_by_crm(DRIVER)
-  DRIVER.get CommonSetting[:url_page_crm] + ""
-  sleep 1
-  DRIVER.find_elements(:css, ".btn-primary")[0].click
-
-end
-
-Given /^Update and set first group by question$/ do
-  authorization_by_crm(DRIVER)
-  DRIVER.get CommonSetting[:url_page_crm] + ""
-  sleep 1
-  DRIVER.find_elements(:css, ".btn-primary")[0].click
-
-end
 
 Given /^Create new mailer$/ do
   authorization_by_crm(DRIVER)
